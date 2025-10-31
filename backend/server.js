272d59953d1ff24fe6ce76a821fffbce831298f6
demonking143s <firedemon143s@gmail.com>
2025-10-31 14:39:20 +0530

    checking

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ app.use(cors({
     origin: [FRONTEND],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    exposedHeaders: ['set-cookie']
 }));
 
 console.log('Frontend URL:', FRONTEND);
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,13 +2,14 @@ import jwt from 'jsonwebtoken'
 
 const generateToken = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET_KEY, {expiresIn: '15d'});
-    res.cookie('sign', token, {
-        maxAge: 15*24*60*1000,
+    const cookieOptions = {
+        maxAge: 15*24*60*60*1000, // 15 days in milliseconds
         httpOnly: true,
-        sameSite: 'lax',
-        secure: process.env.NODE_ENV !== 'development',
-        path: '/'
-    })
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+    };
+res.cookie('sign', token, cookieOptions);
 }
 
 export default generateToken;

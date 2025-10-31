63b5bad0403a533a15549ec9eb06d31b3e13f09d
demonking143s <firedemon143s@gmail.com>
2025-10-31 13:51:07 +0530

    check4

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,8 @@ const generateToken = (userId, res) => {
         maxAge: 15*24*60*1000,
         httpOnly: true,
         sameSite: 'lax',
-        secure: process.env.NODE_ENV !== 'development'
+        secure: process.env.NODE_ENV !== 'development',
+        path: '/'
     })
 }
 

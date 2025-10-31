67a2752d2b9168918bc158335b879979f296fcde
demonking143s <firedemon143s@gmail.com>
2025-10-31 13:58:31 +0530

    check4

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET_KEY, {expiresIn: '15d'});
     res.cookie('sign', token, {
         maxAge: 15*24*60*1000,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: 'lax',
         secure: process.env.NODE_ENV !== 'development',
         path: '/'
